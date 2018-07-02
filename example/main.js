6b399822f1672686d1520921ca6c9fb8968bff93
Pasquale Boemio <pasquale.boemio@statwolf.com>
2018-07-02 21:39:29 +0100

    updated examples

--- example/main.js
+++ example/main.js
@@ -1,1 +1,28 @@
 const simple = require('./simple.html');
+const id = require('incremental-dom');
+
+const data = {
+  items: {
+    first: {
+      text: 'some text'
+    },
+    second: {
+      text: 'other text'
+    }
+  },
+  canShowLove: true,
+  loveIt: function() {
+    alert('<3');
+  }
+};
+
+const root = document.querySelector('body');
+const patch = function() {
+  data.now =  new Date();
+
+  id.patch(root, function() {
+    simple(data);
+  });
+};
+patch();
+setInterval(patch, 1000);
